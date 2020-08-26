72a64b9d9be2fd898dff5f46227e23bd10d8c190
Dylan <69824208+DylanJFitzgerald@users.noreply.github.com>
2020-08-26 16:22:29 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+let decision : Boolean = true
+
+if (decision) {
+    basic.showString("So True",100)
+    basic.showIcon(IconNames.Yes)
+}else{
+    basic.showString("So False",100)
+    basic.showIcon(IconNames.No)
+}
